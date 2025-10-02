0ab87557c3f2ae099f520c11ec60f3a40ef0ca86
poohcom1 <pooh677@gmail.com>
2025-10-03 00:41:55 +0700

    chore: update run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker build . -t oomf --build-arg GET_COMMIT_HASH=$(git rev-parse HEAD)
-docker rm oomf
-docker run --name oomf --env-file .env oomf
+docker build . -t oomf-dev --build-arg GET_COMMIT_HASH=$(git rev-parse HEAD)
+docker rm oomf-dev
+docker run --name oomf-dev --env-file .env oomf-dev
